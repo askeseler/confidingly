e6deeab154c0b7800e61102e840911e3eb428fa7
Michael G <michael.werner.gerstenberger@gmail.com>
2024-09-18 17:31:26 +0200

    scroll-content sizing

--- frontend/src/InfoPage.js
+++ frontend/src/InfoPage.js
@@ -1,4 +1,6 @@
 import React from "react";
+import './InfoPage.css'; // Importing the CSS file for styles
+
 
 class InfoPage extends React.Component {
     constructor(props) {
@@ -6,7 +8,44 @@ class InfoPage extends React.Component {
     }
   
     render() {
-        return <>Info Page</>
+        return <>
+        <div className="scroll-container">
+        <div className="scroll-content">
+          <h1>Lorem Ipsum</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            imperdiet velit nec magna viverra, at accumsan arcu luctus. Integer
+            vel felis et eros tincidunt tincidunt. Etiam auctor lorem ut mi
+            fermentum, ut interdum purus posuere.
+          </p>
+          <p>
+            Suspendisse potenti. Pellentesque habitant morbi tristique senectus et
+            netus et malesuada fames ac turpis egestas. Phasellus non scelerisque
+            ligula. Donec lacinia fermentum lorem sit amet vehicula.
+          </p>
+  
+          {/* Wrap the images in a div */}
+          <div className="image-container">
+            <img src="https://via.placeholder.com/300x200" alt="Placeholder" />
+          </div>
+  
+          <p>
+            Praesent condimentum sapien magna, in vestibulum massa vulputate et.
+            Integer suscipit sollicitudin metus, ut mollis odio vestibulum vel.
+            Fusce accumsan enim eu velit varius malesuada. Aenean varius quam
+            velit, a feugiat metus fermentum non.
+          </p>
+  
+          <div className="image-container">
+            <img src="https://via.placeholder.com/300x200" alt="Placeholder" />
+          </div>
+  
+          <p>
+            Nullam ut dictum magna, non sodales purus. Suspendisse potenti. In non
+            dui eget elit malesuada congue non in massa.
+          </p>
+        </div>
+      </div></>
     }
   }
 export default InfoPage;
